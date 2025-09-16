d717ad443795cf4f6638554f870df6a4e6cd8c66
cram0 <j.augugliaro@outlook.fr>
2025-09-16 16:33:20 +0200

    feat: set navbar

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -2,6 +2,6 @@
 
 <template>
 	<div class="h-14 bg-beige border border-b-2 border-beige-border p-3">
-		<img src="@/assets/images/logo.svg" />
+		<img src="@/assets/images/logo.svg">
 	</div>
 </template>
