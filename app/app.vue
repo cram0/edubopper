729cb86a3a82ab45b1447ebf27569b380731d042
cram0 <j.augugliaro@outlook.fr>
2025-09-16 16:31:55 +0200

    style: align content to the middle with correct padding

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-	<Navbar />
-	<div class="min-h-dvh bg-beige p-4">
-		<NuxtPage />
+	<div class="min-h-dvh flex flex-col bg-beige">
+		<Navbar />
+		<div class="p-4 flex flex-col flex-1 max-w-3xl w-full mx-auto">
+			<NuxtPage />
+		</div>
 	</div>
 </template>
