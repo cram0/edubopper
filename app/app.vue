79c1526b6f2e2a5ebbc9fe5cb55ff40f1c494096
cram0 <j.augugliaro@outlook.fr>
2025-09-16 11:02:00 +0200

    Create: Initial files

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+	<Navbar />
+	<div class="min-h-dvh bg-beige p-4">
+		<NuxtPage />
+	</div>
+</template>
---
+++ app/components/Navbar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="h-14 bg-beige border border-b-2 border-beige-border p-3">
+		<img src="@/assets/images/logo.svg" />
+	</div>
+</template>
